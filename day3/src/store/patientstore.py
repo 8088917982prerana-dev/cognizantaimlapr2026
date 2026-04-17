3d098bfcd25b26401a7dd5a15c6e8dfd09d68aae
8088917982prerana-dev <8088917982prerana@gmail.com>
2026-04-17 04:03:04 +0000

    updated

--- day3/src/store/patientstore.py
+++ day3/src/store/patientstore.py
@@ -1,47 +1,21 @@
-"""create doctor crud operation"""
-import sys
-import os
-from models.doctor import Doctor
-from exceptions.doctor_not_found_exception import DoctorNotFoundException
-# Add project root to Python path
-project_root = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..', '..')
-)
-
-sys.path.append(project_root)
-
-from conf.logger_conf import setup_logger
-logger = setup_logger()
-
-class DoctorStore:
+class patientstore:
     def __init__(self):
-        self.doctors = []
-    
-    def add_doctor(self, doctor: Doctor):
-        logger.info(f"Adding doctor: {doctor}")
-        self.doctors.append(doctor)
-
-    def get_all_doctors(self):
-        logger.info("Fetching all doctors")
-        return self.doctors
-    
-    def get_doctor_by_id(self, doctor_id: int) -> Doctor:
-        logger.info(f"Fetching doctor with ID: {doctor_id}")
-        for doctor in self.doctors:
-            if doctor.id == doctor_id:
-                return doctor
-        raise DoctorNotFoundException(f"Doctor with ID {doctor_id} not found") 
-        
-    
-    def update_doctor(self, doctor_id: int, name: str = None, specialization: str = None):
-        doctor = self.get_doctor_by_id(doctor_id)
-        logger.info(f"Updating doctor with ID: {doctor_id}")
-        if doctor:
+        self.patients = []
+    def add_patient(self, patient):
+        self.patients.append(patient)
+    def get_all_patients(self):
+        return self.patients
+    def get_patient_by_id(self, patient_id):
+        for patient in self.patients:
+            if patient.id == patient_id:
+                return patient
+        raise PatientNotFoundException(f"Patient with ID {patient_id} not found")
+    def update_patient(self, patient_id, name=None, age=None):
+        patient = self.get_patient_by_id(patient_id)
+        if patient:
             if name:
-                doctor.name = name
-            if specialization:
-                doctor.specialization = specialization
-    
-    def delete_doctor(self, doctor_id: int):
-        logger.info(f"Deleting doctor with ID: {doctor_id}")
-        self.doctors = [doctor for doctor in self.doctors if doctor.id != doctor_id
+                patient.name = name
+            if age:
+                patient.age = age
+    def delete_patient(self, patient_id):
+        self.patients = [patient for patient in self.patients if patient.id != patient_id]
