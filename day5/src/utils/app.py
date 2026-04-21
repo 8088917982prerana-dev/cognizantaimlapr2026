a10213d4d5e52ada42629cfc54b58f2e0832d973
8088917982prerana-dev <8088917982prerana@gmail.com>
2026-04-21 09:42:47 +0000

    updated

--- day5/src/utils/app.py
+++ day5/src/utils/app.py
@@ -2,11 +2,15 @@
 import sys
 import os
 from faker import Faker
+ 
+ 
+ 
+ 
 # Add project root to Python path
 project_root = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', '..')
 )
-
+ 
 sys.path.append(project_root)
 from src.configurations.conf import Config
 from src.dataloaders.customer_csv_data_loader import CustomerCSVDataLoader
@@ -27,13 +31,33 @@ def display_customers(customer_store):
       print(f"email: {customer.email}")
       print(f"phone_no: {customer.phone_no}")
       print("-------------")
-          
-if __name__ == "__main__":   
+         
+def update_customer(customer_store,customer_id):
+    customer=customer_store.get_customer(customer_id)
+    faker = Faker()
+    customer.name.first_name = faker.first_name()
+    customer.name.last_name = faker.last_name()
+    customer.email = faker.email()
+    customer.phone_no = faker.random_int(min=1000000000, max=9999999999)
+    customer_store.update_customer(customer_id, customer)
+    return customer_store
+ 
+def get_customer_by_id(customer_store, customer_id):
+    customer = customer_store.get_customer(customer_id)
+    print(f"customer_id: {customer.customer_id}")
+    print(f"name: {customer.name.first_name} {customer.name.last_name}")    
+    print(f"email: {customer.email}")
+    print(f"phone_no: {customer.phone_no}")
+    print("--------------------")
+ 
+   
+if __name__ == "__main__":  
    customer_store = CustomerStoreImpl()
-   pipeline = PipelineRunner()
-   pipeline.add_stage(load_customers)
-   pipeline.add_stage(display_customers)
-   pipeline.run(customer_store)
-   
-   
-      
+   pipeline_runner = PipelineRunner()
+   pipeline_runner.add_stage(load_customers(customer_store))
+   pipeline_runner.add_stage(display_customers(customer_store))
+   pipeline_runner.add_stage(update_customer(customer_store, 1))
+   pipeline_runner.add_stage(get_customer_by_id(customer_store, 1))
+   pipeline_runner.run()
+ 
+ 
--- day5/src/utils/pipeline_runner.py
+++ day5/src/utils/pipeline_runner.py
@@ -1,11 +1,9 @@
-class  PipelineRunner:
+class PipelineRunner:
     def __init__(self):
-        self.stages = []
-
+        self.stages= []
     def add_stage(self, stage):
         self.stages.append(stage)
-
-    def run(self, data):
-        for step in self.stages:
-            data = step(data)
-        return data
+    def run(self):
+        for stage in self.stages:
+            stage
+ 
