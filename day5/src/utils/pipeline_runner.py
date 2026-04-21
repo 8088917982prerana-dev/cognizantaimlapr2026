646c16e8b1a7f46134fde9930108d7cf96d81166
8088917982prerana-dev <8088917982prerana@gmail.com>
2026-04-21 10:55:25 +0000

    updated

--- day5/src/utils/pipeline_runner.py
+++ day5/src/utils/pipeline_runner.py
@@ -3,7 +3,7 @@ class PipelineRunner:
         self.stages= []
     def add_stage(self, stage):
         self.stages.append(stage)
-    def run(self):
+    def run(self,**kwargs):
         for stage in self.stages:
-            stage
- 
+            data=stage(**kwargs)
+        return data
